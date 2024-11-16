8c8b31ce264e8e06e4b8f61c6ba6bf37bfd40e7d
Cole Essig <hoopwalker@yahoo.com>
2024-11-16 14:35:33 -0700

    Added a protected route system, created app context for the login, set basic intro page layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Register from "../Register/Register";
@@ -8,12 +8,46 @@ import JoinPlan from "../JoinPlan/JoinPlan";
 import DishPicker from "../DishPicker/DishPicker";
 import Review from "../Review/Review";
 import Home from "../Home/Home"
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute"
+import AppContext from '../../context/AppContext';
 import './App.css'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return ( 
-    <>
-    </>
+    <div className='page'>
+     <div className="page__content">
+      <AppContext.Provider value={isLoggedIn} >
+        <Routes>
+          <Route
+            path="/home"
+            element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+          />
+          <Route 
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="*"
+            element={
+              isLoggedIn ? (
+                <Navigate to="/home" replace />
+              ) : (
+                <Navigate to="/register" replace />
+              )
+            }
+          />
+        </Routes>
+      </AppContext.Provider>
+     </div>
+    </div>
   )
 }
 
