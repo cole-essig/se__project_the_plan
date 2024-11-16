9e753234ed1592dc5bca0d881620508dace18afd
Cole Essig <hoopwalker@yahoo.com>
2024-11-16 15:07:19 -0700

    updated main.jsx to user browser router, added content to register. Error on load

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import "./Register.css";
+
+function Register() {
+  return (
+    <div className="register"> 
+      <h2 className="register__title">What up World!?!?</h2>
+    </div>
+  )
+}
+
+export default Register
