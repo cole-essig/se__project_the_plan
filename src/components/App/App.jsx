87e4578588502fb9a07efc68ba0f6d209bd041da
Cole Essig <hoopwalker@yahoo.com>
2024-11-16 14:04:10 -0700

    Added more file structure, created most of main components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Register from "../Register/Register";
+import JoinPlan from "../JoinPlan/JoinPlan";
+import DishPicker from "../DishPicker/DishPicker";
+import Review from "../Review/Review";
+import Home from "../Home/Home"
 import './App.css'
 
 function App() {
-  return (
+  return ( 
     <>
     </>
   )
