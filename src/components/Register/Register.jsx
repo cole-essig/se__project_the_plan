7be5b2e9773c5146f677515e098d00103508c317
Cole Essig <hoopwalker@yahoo.com>
2024-11-16 21:37:06 -0700

    Added register page markup and normalize css

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,9 +3,46 @@ import { useState } from "react";
 import "./Register.css";
 
 function Register() {
+const [name, setName] = useState('');
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+  const [password, setPassword] = useState('');
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+  
   return (
     <div className="register"> 
-      <h2 className="register__title">What up World!?!?</h2>
+      <h2 className="register__title">Sign Up</h2>
+      <div className="register__form">
+      <label htmlFor='name' className='register__label'>
+                Name{" "}
+                <input 
+                    type='text'
+                    className='register__input'
+                    id='name'
+                    placeholder='Name'
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </label>
+            <label htmlFor='password' className='register__label'>
+                Password{" "}
+                <input 
+                    type='password'
+                    className='register__input'
+                    id='password'
+                    placeholder='Password'
+                    onChange={handlePasswordChange}
+                    value={password}
+                />
+            </label>
+      </div>
+      <div className='register__buttons'>
+            <button type='submit' className="register__submit-button">Create Plan</button>
+            <button className="register__switch-button">Join Dinner</button>
+      </div>
     </div>
   )
 }
