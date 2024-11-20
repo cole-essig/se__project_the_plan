26b11349aa18dc7a9641ec2ea1de607199d2d053
Cole Essig <hoopwalker@yahoo.com>
2024-11-20 11:43:08 -0700

    Added a click function to pin cards if clicked on the recipe page, now can track recipes see whats been covered

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../../assets/img/logo.svg";
 import "./RecipeCard.css"
 
 function RecipeCard({ item, cardDelete }) {
+  const [isClicked, setIsClicked] = useState(false);
+  const onClick = () => {
+    setIsClicked(!isClicked);
+  }
   const handleCardDelete = () => {
     cardDelete(item.id);
   };
 
   return (
-    <li className="card">
+    <li className="card" onClick={onClick}>
+      <img src={Logo} alt="turkey logo" className={`card__pin ${isClicked ? "card__pin_open" : ''}`} />
       <button type='button' className='card__delete' onClick={handleCardDelete}></button>
       <p className="card__name">{item.dish}</p>
       <p className="card__servings">{`Servings: ${item.servings}`}</p>
