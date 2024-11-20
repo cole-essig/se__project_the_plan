552bd25189538d3057f64d098f1de6f080ff759e
bjthe4 <bjthe4@gmail.com>
2024-11-20 11:43:20 -0600

    save current changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,63 +1,63 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import Register from "../Register/Register";
 import JoinPlan from "../JoinPlan/JoinPlan";
-import CreatePlan from '../CreatePlan/CreatePlan';
-import Home from "../Home/Home"
-import ProtectedRoute from "../ProtectedRoute/ProtectedRoute"
-import AppContext from '../../context/AppContext';
-import './App.css'
+import CreatePlan from "../CreatePlan/CreatePlan";
+import Home from "../Home/Home";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import AppContext from "../../context/AppContext";
+import "./App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
 
-  return ( 
-    <div className='page'>
-     <div className="page__content">
-     <AppContext.Provider value={isLoggedIn} >
-        <Routes>
-          <Route
-            path="/home"
-            element={
-            <ProtectedRoute anonymous>
-              <Home />
-            </ProtectedRoute>
-          }
-          />
-          <Route 
-            path="/home/create-plan"
-            element={
-              <ProtectedRoute anonymous>
-                <CreatePlan />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/register"
-            element={<Register />}
-          />
-          <Route 
-            path="/login"
-            element={<JoinPlan />}
-          />
-          <Route
-            path="*"
-            element={
-              isLoggedIn ? (
-                <Navigate to="/home" replace />
-              ) : (
-                <Navigate to="/register" replace />
-              )
-            }
-          />
-        </Routes>
-      </AppContext.Provider>
-     </div>
+  return (
+    <div className="page">
+      <div className="page__content">
+        <AppContext.Provider value={isLoggedIn}>
+          <Routes>
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute anonymous>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/home/create-plan"
+              element={
+                <ProtectedRoute anonymous>
+                  <CreatePlan />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<JoinPlan />} />
+            <Route
+              path="*"
+              element={
+                isLoggedIn ? (
+                  <Navigate to="/home" replace />
+                ) : (
+                  <Navigate to="/register" replace />
+                )
+              }
+            />
+          </Routes>
+        </AppContext.Provider>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
